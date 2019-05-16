69d1efbb4c28f0522155069f8f7181e08412231e
lui914 <luirangel914@gmail.com>
2019-05-15 19:46:43 -0500

    added reviews, shoes size, and colors

--- dataparser.js
+++ dataparser.js
@@ -1,27 +1,48 @@
-const fs = require('fs');
+const fs = require("fs");
+const { reviews } = require("./reviews");
 
-fs.readFile('./shoes.csv', 'utf8', (err, fileRes) => {
-    if (err) {
-        console.log('error reading file');
-    } else {
-        const shoeArray = [];
-        const rows = fileRes.split('\n');
-        const headers = rows[1].split(',');
+fs.readFile("./shoes.csv", "utf8", (err, fileRes) => {
+  if (err) {
+    console.log("error reading file");
+  } else {
+    const shoeArray = [];
+    const rows = fileRes.split("\n");
+    const headers = rows[1].split(",");
 
-        for (let i = 2; i < rows.length; i++) {
-            let currShoe = {};
-            let shoeDesc = rows[i].split(',');
+    for (let i = 2; i < rows.length; i++) {
+      let currShoe = {};
+      let shoeDesc = rows[i].split(",");
 
-            for (let j = 0; j < shoeDesc.length; j++) {
-                currShoe[headers[j]] = shoeDesc[j];
+      for (let j = 0; j < shoeDesc.length; j++) {
+        currShoe[headers[j]] = shoeDesc[j];
 
-                if (headers[j] === 'images') {
-                    currShoe[headers[j]] = shoeDesc[j].split(' ');
-                }
-                // need to add reviews column to data (reviews: [{user: STRING, date: DATE, stars: NUMBER, title: STRING, description: STRING},...]);
-            }
-            shoeArray.push(currShoe);
+        if (headers[j] === "images") {
+          currShoe[headers[j]] = shoeDesc[j].split(" ");
         }
-        console.log(shoeArray);
+        currShoe["reviews"] = reviews;
+        currShoe["colors"] = [
+          "white",
+          "black",
+          "red",
+          "orange",
+          "yellow",
+          "pink",
+          "grey",
+          "green",
+          "magenta",
+          "brown",
+          "silver",
+          "tan",
+          "rainbow",
+          "gold",
+          "purple",
+          "neon"
+        ];
+        // need to add reviews column to data (reviews: [{user: STRING, date: DATE, stars: NUMBER, title: STRING, description: STRING},...]);
+        currShoe["sizes"] = [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12];
+      }
+      shoeArray.push(currShoe);
     }
+    console.log(shoeArray);
+  }
 });
---
+++ reviews.js
@@ -0,0 +1,87 @@
+const reviews = [
+  {
+    user: "Elita Q.",
+    date: "Apr 29, 2019",
+    stars: 5,
+    title: "such wow",
+    description:
+      " Absolutely wonderful! Really good.If you aren't sure, always go for Fike shoes. I wish I would have thought of it first."
+  },
+
+  {
+    user: "Bobby C.",
+    date: "Apr 24, 2019",
+    stars: 5,
+    title: "much wow",
+    description: "Fike shoes impressed me on multiple levels."
+  },
+
+  {
+    user: "Benita B.",
+    date: "Apr 22, 2019",
+    stars: 5,
+    title: "AMAZING!!!",
+    description: " was amazed at the quality of Fike shoes."
+  },
+
+  {
+    user: "Elita Q.",
+    date: "Apr 29, 2019",
+    stars: 5,
+    title: "such wow",
+    description:
+      " Absolutely wonderful! Really good.If you aren't sure, always go for Fike shoes. I wish I would have thought of it first."
+  },
+
+  {
+    user: "Squeeps Z.",
+    date: "Apr 20, 2019",
+    stars: 4,
+    title: "righteous",
+    description: " I'm flying high with these shoes! "
+  },
+
+  {
+    user: "Biggie S",
+    date: "Apr 20, 2019",
+    stars: 3,
+    title: `c'mon man`,
+    description: " They aight"
+  },
+
+  {
+    user: "Two.pac",
+    date: "Apr 10, 2019",
+    stars: 2,
+    title: "Dissapointed",
+    description: "Fike more like SIKE!!!!"
+  },
+
+  {
+    user: "FootFreak",
+    date: "Feb 24, 2019",
+    stars: 4,
+    title: "pretty good",
+    description: " Can't complain Fike gets the job done. "
+  },
+
+  {
+    user: "PostMalone",
+    date: "Feb 20, 2019",
+    stars: 5,
+    title: "Great Product",
+    description:
+      " Wore these to an interview and instantly got a job thanks FIKE"
+  },
+
+  {
+    user: "Water Malone",
+    date: "Feb 18, 2019",
+    stars: 5,
+    title: "Juicy",
+    description:
+      " Fike is always dripping swagoo I love their products keep it up"
+  }
+];
+
+module.exports.reviews = reviews;
